a442484a7e7870eb8d6b3da2a436d43c3089100c
Tao Tao <z5361154@ad.unsw.edu.au>
2024-02-02 03:50:03 +1100

    Removed old theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,60 @@
 import React from 'react';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import { Link } from 'react-router-dom';
+import {
+  Link, 
+  useLocation
+} from 'react-router-dom';
 
 function SignInForm () {
   const smallScreen = useMediaQuery('(max-width: 650px)');
   const largeScreen = useMediaQuery('(min-width: 651px)');
+  const location = useLocation();
+
+  const isActive = (path) => location.pathname === path;
 
   return (
     <>
       {smallScreen && (
         <div className="navbar-mobile">
-          <span className="navbar-mobile-items"><Link to="/projects" className="link">Projects</Link></span>
-          <span className="navbar-mobile-items"><Link to="/" className="link">Home</Link></span>
-          <span className="navbar-mobile-items"><Link to="/about" className="link">About</Link></span>
+          <span className={`navbar-mobile-items ${isActive('/projects') && 'active'}`}>
+            <Link to="/projects" className="link">
+              PROJECTS
+            </Link>
+          </span>
+          <span className={`navbar-mobile-items ${isActive('/') && 'active'}`}>
+            <Link to="/" className="link">
+              HOME
+            </Link>
+          </span>
+          <span className={`navbar-mobile-items ${isActive('/about') && 'active'}`}>
+            <Link to="/about" className="link">
+              ABOUT
+            </Link>
+          </span>
         </div>
       )}
       {largeScreen && (
         <div className="navbar-desktop">
-          <span className="navbar-desktop-items"><Link to="/projects" className="link">Projects</Link></span>
-          <span className="navbar-desktop-items"><Link to="/" className="link">Home</Link></span>
-          <span className="navbar-desktop-items"><Link to="/about" className="link">About</Link></span>
+          <span className={`navbar-desktop-items ${isActive('/projects') && 'active'}`}>
+            <Link to="/projects" className="link">
+              PROJECTS
+            </Link>
+          </span>
+          <span className={`navbar-desktop-items ${isActive('/') && 'active'}`}>
+            <Link to="/" className="link">
+              HOME
+            </Link>
+          </span>
+          <span className={`navbar-desktop-items ${isActive('/about') && 'active'}`}>
+            <Link to="/about" className="link">
+              ABOUT
+            </Link>
+          </span>
         </div>
       )}
     </>
-  )
+  );
 }
+
 
 export default SignInForm;
