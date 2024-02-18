b09ea69e0830fcf0f4141245f5e04f8aa436b1bc
Tao Tao <z5361154@ad.unsw.edu.au>
2024-02-18 20:39:12 +1100

    Created new project tiles

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Navbar from '../components/Navbar';
+import ProjectCard from '../components/ProjectCard';
 import SocialLinks from '../components/SocialLinks';
-
 
 const Projects = () => {
   const smallScreen = useMediaQuery('(max-width: 650px)');
@@ -22,6 +22,38 @@ const Projects = () => {
       )}
       {largeScreen && (
         <>
+          <div className="project-tiles-desktop">
+            <ProjectCard>
+                <div className="blur-container"></div>
+                <div className="project-tile">
+                  <div className="project-card-title simulated-ecology">
+                    <h2 className="project-img-desktop-text padding highlight">Simulated Ecology</h2>
+                  </div>
+                  <div className="project-card-info padding">
+                    <p>CATEGORY: 3D Design, Interactive Environment</p>            
+                    <p>Simulated Ecology was my major project for my 3D visualisation course. It is an original 3D realtime interactive environment
+                      which depicts the futuristic possibility of only being able to recreate nature in a digital environment. This project was
+                      made in Unity over the course of 6 weeks. 
+                    </p>
+                  </div>
+              </div>
+              </ProjectCard>
+              <ProjectCard>
+                <div className="blur-container"></div>
+                <div className="project-tile">
+                  <div className="project-card-title lurkforwork">
+                    <h2 className="project-img-desktop-text padding highlight">LurkForWork</h2>
+                  </div>
+                  <div className="project-card-info padding">
+                    <p>CATEGORY: 3D Design, Interactive Environment</p>            
+                    <p>Simulated Ecology was my major project for my 3D visualisation course. It is an original 3D realtime interactive environment
+                      which depicts the futuristic possibility of only being able to recreate nature in a digital environment. This project was
+                      made in Unity over the course of 6 weeks. 
+                    </p>
+                  </div>
+              </div>
+              </ProjectCard>
+          </div>
           <footer>
             <SocialLinks />
           </footer>
